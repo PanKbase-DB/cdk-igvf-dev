200324c87c82a0b1d415bc695904ec6a2816c9f2
keenangraham <ksgraham@stanford.edu>
2022-08-17 17:38:59 -0700

    add-slack-incoming-webhook (#2)

--- notification/cdk_igvf_dev/notification_stack.py
+++ notification/cdk_igvf_dev/notification_stack.py
@@ -4,6 +4,8 @@ from constructs import Construct
 
 from aws_cdk.aws_chatbot import SlackChannelConfiguration
 
+from cdk_igvf_dev.constructs.slack import SlackWebhook
+
 from typing import Any
 
 
@@ -17,4 +19,8 @@ class NotificationStack(cdk.Stack):
             slack_channel_configuration_name='aws-chatbot',
             slack_workspace_id='T1KMV4JJZ',
             slack_channel_id='C034GTRCCLU',
+        )
+        self.encode_dcc_slack_webhook: SlackWebhook = SlackWebhook(
+            self,
+            'AwsIgvfDevSlackWebhook',
         )
