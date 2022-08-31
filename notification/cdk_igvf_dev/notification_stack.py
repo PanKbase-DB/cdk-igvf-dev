9d44c3a784cf312446c8b394e4e3ac4f29585c4f
keenangraham <ksgraham@stanford.edu>
2022-08-31 14:57:30 -0700

    add-alarm-notification-topic (#5)

--- notification/cdk_igvf_dev/notification_stack.py
+++ notification/cdk_igvf_dev/notification_stack.py
@@ -4,6 +4,8 @@ from constructs import Construct
 
 from aws_cdk.aws_chatbot import SlackChannelConfiguration
 
+from aws_cdk.aws_sns import Topic
+
 from cdk_igvf_dev.constructs.slack import SlackWebhook
 
 from typing import Any
@@ -19,6 +21,13 @@ class NotificationStack(cdk.Stack):
             slack_channel_configuration_name='aws-chatbot',
             slack_workspace_id='T1KMV4JJZ',
             slack_channel_id='C03TCFF0MTM',
+        )
+        self.alarm_notification_topic = Topic(
+            self,
+            'AlarmNotificationTopic',
+        )
+        self.encode_dcc_chatbot.add_notification_topic(
+            self.alarm_notification_topic
         )
         self.encode_dcc_slack_webhook: SlackWebhook = SlackWebhook(
             self,
